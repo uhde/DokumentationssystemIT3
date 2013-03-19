34a282c3b1cebc78cf0e42adc9a855e3fe6df4e8
uhde <micha.grueninger@uhde.de>
2013-03-19 09:39:32 +0100

    Revert "Verzeichniss abhängigkeiten versucht zu beseitigen"
    This reverts commit a98701e77b1f70737981edfdb1a75d06d8940f9c.

--- layout/kundenwahl.lay.php
+++ layout/kundenwahl.lay.php
@@ -10,7 +10,7 @@
 		<a href="index.php?knd_id={$id}&page=server" target="_top">{$name}</a>
 	</td>
     <td align="left" style="padding-left:3px;padding-top:3px;padding-bottom:3px;">
-		<a href="userconf/kunden_benutzerdefiniert_verstecken.php?knd_id={$id}&site=/../index.php">
+		<a href="userconf/kunden_benutzerdefiniert_verstecken.php?knd_id={$id}&site=/dokuit3/index.php">
             <img src="syspics/user_invisible.png" style="border:none:width:12px;height:12px;" alt="Kunde Verstecken"></a>
     </td>
 	<td align="left" style="padding-left:3px;padding-top:3px;padding-bottom:3px;">
@@ -28,7 +28,7 @@
             <img src="syspics/new_entry.png" style="width:24px;height:20px;border:none" alt="Neuer Eintrag" title="Neuer Eintrag">
         </a>
     
-        <a href="userconf/kunden_anzeigen.php?site=/../index.php"  > 
+        <a href="userconf/kunden_anzeigen.php?site=/dokuit3/index.php"  > 
             <img src="syspics/kunde.png" style="width:24px;height:20px;border:none" alt="Alle Kunden anzeigen" title="Kunden anzeigen/verstecken">
             <img src="syspics/switch.png" style="width:24px;height:20px;border:none" alt="Alle Kunden anzeigen" title="Kunden anzeigen/verstecken">
         </a>
