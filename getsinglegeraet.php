ff501d9dc84281565072d2cf9945942a3f3310d6
uhde <micha.grueninger@uhde.de>
2013-03-21 14:42:21 +0100

    kodierungsinfortmationen eingebaut

--- getsinglegeraet.php
+++ getsinglegeraet.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+<body>
 <?php
 /*
 error_reporting(E_ALL);
@@ -164,3 +170,5 @@ function MakeButtons($Data, $adresse){
 	
 }
 ?>
+</body>
+</html>
