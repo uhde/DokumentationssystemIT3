62182a1dca2b4ea22f8eae6d25749c1567c1c66d
uhde <micha.grueninger@uhde.de>
2013-03-21 15:46:26 +0100

    noch ein fix für den vorhergegangen fix

--- layout/geraete_general.lay.php
+++ layout/geraete_general.lay.php
@@ -47,9 +47,7 @@
 	</tr>
 	<tr id="trinfo{$id}" class="TRInfo">
 		<!--<td colspan="5" style="background-color:#fff;border-bottom:1px solid #333;padding-left:0px;padding-right:0px;padding-bottom:0px;border-top:1px solid #ddd;">-->
-		<td colspan="6" class="info_schatten4">
-			{$InfoTable}
-		</td>
+		
 	</tr>
 
 !#----- Data:End -----#!
