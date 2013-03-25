367019441d90f045cbadcc149ec23769d8d52aac
uhde <micha.grueninger@uhde.de>
2013-03-25 14:39:33 +0100

    testfile aktualisiert

--- test.php
+++ test.php
@@ -15,8 +15,8 @@ function loadXMLDoc(Test,id)
             var aufzurufende_id;
             aufzurufende_id = "#"+Test;
             $(aufzurufende_id).empty();
-            //$(aufzurufende_id).html(xmlhttp.responseText);
-            document.getElementById(Test).innerHTML = "Dies ist ein test";
+            $(aufzurufende_id).html(xmlhttp.responseText);
+            //document.getElementById(Test).innerHTML = "Dies ist ein test";
             alert(xmlhttp.responseText);
         }
     }
@@ -34,7 +34,7 @@ function loadXMLDoc(Test,id)
 <table>
 <tr id="Info5" class="Data0" onmouseover="this.className='Aktiv'"  onmouseout="this.className='Data0'" >
     <td class="slider" value="#trinfo5" onclick="loadXMLDoc('trinfo5','5')">
-        {$name}
+        name
     </td>
 </tr>
 <tr id="trinfo5" class="TRInfo">
