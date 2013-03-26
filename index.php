a67e39b2337530f8312670770d77843164ddfc2f
uhde <micha.grueninger@uhde.de>
2013-03-26 11:54:24 +0100

    wegen merge wieder rausgeschmissen

--- index.php
+++ index.php
@@ -86,7 +86,6 @@ if (file_exists($page.'.php')) {
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<meta http-equiv="x-ua-compatible" content="ie=8">
 <span style="font-family: Verdana, Arial, Helvetica, sans-serif;">
 <title>DokuIT - Dokumentationsverwaltung v3</title>
 </span>
