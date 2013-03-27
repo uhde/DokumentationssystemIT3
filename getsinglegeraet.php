4d66ab1384ad9dfba87d83c01ebce2f376ddb959
uhde <micha.grueninger@uhde.de>
2013-03-27 15:26:33 +0100

    test codierung bei gerätetest

--- getsinglegeraet.php
+++ getsinglegeraet.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html >
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <link rel="stylesheet" href="css/styles.css" type="text/css" />
 <link rel="stylesheet" href="css/reset.css" type="text/css" />
 </head>
