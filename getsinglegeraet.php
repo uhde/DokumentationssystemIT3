5a275d92f0c0517c08fc5c5cbad581eda3a234c2
uhde <micha.grueninger@uhde.de>
2013-03-27 15:21:33 +0100

    test

--- getsinglegeraet.php
+++ getsinglegeraet.php
@@ -1,7 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!DOCTYPE html >
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="css/styles.css" type="text/css" />
+<link rel="stylesheet" href="css/reset.css" type="text/css" />
 </head>
 <body>
 <?php
