1cf7d0a64e4905c43754f942dae43f1e0bc0b0a3
uhde <micha.grueninger@uhde.de>
2013-03-27 15:30:27 +0100

    test codierung

--- getsinglegeraet.php
+++ getsinglegeraet.php
@@ -1,7 +1,7 @@
 ﻿<!DOCTYPE html >
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
 <link rel="stylesheet" href="css/styles.css" type="text/css" />
 <link rel="stylesheet" href="css/reset.css" type="text/css" />
 </head>
