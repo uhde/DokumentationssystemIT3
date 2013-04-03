536d1b6dd4ac348df29f06f269c6829445a28049
uhde <micha.grueninger@uhde.de>
2013-04-03 14:55:34 +0200

    test mit geraeteedit

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <html>
 <head>
  <meta charset="utf-8">
-<script type='text/javascript' src='js/jquery-1.9.1.min.js'></script>+
+<script type='text/javascript' src='js/jquery-1.9.1.min.js'></script>
 <link rel="stylesheet" href="js/fancyapps-fancyBox-0ffc358/source/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
 <script type='text/javascript' src='js/fancyapps-fancyBox-0ffc358/source/jquery.fancybox.js'></script>
 <script type='text/javascript'>
@@ -28,6 +28,16 @@ $(document).ready(function() {
 <form>
 <div id="Buchungen">
 <table>
+
+
+<a class="various" href="geraeteedit.php?kunde=5&id=5&mode=edit">link</a>
+
+
+
+<br>
+<br>
+<br>
+<br>
 <ul class="list">
 	<li>
 		<a class="various fancybox.ajax" href="/demo/ajax.php">Ajax</a>
