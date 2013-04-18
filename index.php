5fdce11de596f2b1494b0bf7704ed39eb5bbc7e5
uhde <micha.grueninger@uhde.de>
2013-04-18 10:48:30 +0200

    versuch Doctype Error im ie8 zu beheben

--- index.php
+++ index.php
@@ -74,7 +74,8 @@ if (file_exists($page.'.php')) {
 	}
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
