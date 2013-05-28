6b842427ddce42b84de4d6f112d6ac78407d8211
uhde <micha.grueninger@uhde.de>
2013-05-28 09:52:04 +0200

    fix d4ff5a1d72651e2ba247682b05f2643200ad87db

--- include/getsingle_zugang.php
+++ include/getsingle_zugang.php
@@ -77,7 +77,7 @@ include_once("functions.inc.php");
         echo '<tr>
                 <td class="Key">Link (kopieren): </td>
                 <td class="Value">
-                    <input type="text">'.$sqldata["url_text"].'</input>
+                    <input type="text" value="'.$sqldata["url_text"].'"></input>
                 </td>
             <tr>';
     }
