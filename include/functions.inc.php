0418e0fb33a62f7b4d83bda093bc99de6ae140ef
uhde <micha.grueninger@uhde.de>
2013-06-10 11:17:22 +0200

    test

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -3,10 +3,10 @@
 function link_klickbar_machen($text) {
 //http://www.php-einfach.de/tuts_php_links.php
     //Aus http://url wird [URL]http://url[/URL]
-    //$urlsuch[]="/([^]_a-z0-9-=\"'\/])(\\\\[a-z]:)([^ \r\n\(\)\^\$!`\"'\|\[\]\{\}<>]*)/si";
-    $urlsuch[]="/^(\\[\\]{+}[a-z]:)([ \r\n\(\)\^\$!`\"'\|\[\]\{\}<>]*)/si";
-    //$urlreplace[]="\\1[URL]\\2\\4[/URL]";
-    $urlreplace[]="[URL]\\1\\3[/URL]";
+    $urlsuch[]="/([^]_a-z0-9-=\"'\/])(\\\\[a-z]:)([^ \r\n\(\)\^\$!`\"'\|\[\]\{\}<>]*)/si";
+    //$urlsuch[]="/^(\\[\\]{+}[a-z]:)([ \r\n\(\)\^\$!`\"'\|\[\]\{\}<>]*)/si";
+    $urlreplace[]="\\1[URL]\\2\\4[/URL]";
+    //$urlreplace[]="[URL]\\1\\3[/URL]";
     //Aus klaus@mustermann.de wird [EMAIL]klaus@mustermann.de [/EMAIL]
     $emailsuch[]="/([\s])([_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,}))/si"; 
     $emailsuch[]="/^([_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,}))/si"; 
