922e64238f40418f98af46aa39be914fe0a9be99
uhde <micha.grueninger@uhde.de>
2013-06-18 16:06:23 +0200

    test

--- include/getsingle_zugang.php
+++ include/getsingle_zugang.php
@@ -46,7 +46,7 @@ $browser = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
             }
         }
     }
-    $ax_link='\''.$browser.'\' google.de';
+    $ax_link='\\\''.$browser.'\\\' google.de';
     // Sollte eine URL länger als 50 Zeichen lang sein , wird der sichtbare Bereich aus Layoutgründen
     // auf 47 Zeichen gefolgt von einem ... begrenzt.
     if (strlen($sqldata['url'])>50) {
