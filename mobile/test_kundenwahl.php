57693978e845eb01f7c93a09680849035dc5d465
uhde <micha.grueninger@uhde.de>
2013-07-01 15:10:34 +0200

    asdf

--- mobile/test_kundenwahl.php
+++ mobile/test_kundenwahl.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html> 
 <html> 
 <head> 
-	<title>My Page</title> 
+	<title>Kundenwahl</title> 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.css" />
 	<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
