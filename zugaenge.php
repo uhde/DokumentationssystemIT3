4876f86f7ad091a43b3e9c4a4f151d0c5c836fba
uhde <micha.grueninger@uhde.de>
2013-07-11 15:12:44 +0200

    temp test

--- zugaenge.php
+++ zugaenge.php
@@ -48,7 +48,7 @@ if(isset($mode['suche'])&&(!empty($mode['suche']))) {
    }else{
         $sql = "SELECT * FROM ".TBL_ZUGAENGE." WHERE kunde=".MySQL::SQLValue($_SESSION['knd_id'])." ORDER BY titel ".$_SESSION['sort_order'];
    }
-   // echo $sql;
+    echo $sql;
 }
 $arrData_zugaenge=$objMySQL->QueryArray ($sql);
 if ($arrData_zugaenge!==FALSE) {
