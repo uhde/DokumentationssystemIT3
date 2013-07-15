fa882d3c363c448dcaac4ee2775278f1ebd599b6
uhde <micha.grueninger@uhde.de>
2013-07-15 11:27:38 +0200

    config nochmal geändert

--- js/ckeditor/config.js
+++ js/ckeditor/config.js
@@ -13,16 +13,16 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'forms' },
-		{ name: 'basicstyles', groups: [ 'basicstyles' ] },
+		//{ name: 'forms' },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'indent' ] },
 		//{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'tools' },
-		{ name: 'others' },
-		{ name: 'about' }
+		//{ name: 'links' },
+		//{ name: 'insert' },
+		//{ name: 'styles' },
+		//{ name: 'colors' },
+		//{ name: 'tools' },
+		//{ name: 'others' },
+		//{ name: 'about' }
 	];
 
 	// The default plugins included in the basic setup define some buttons that
