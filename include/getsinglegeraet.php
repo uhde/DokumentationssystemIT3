f6e5fca8adb5d59d03457c412893c3a6975838d8
uhde <micha.grueninger@uhde.de>
2013-07-15 11:15:14 +0200

    kursive Schrift eingefügt

--- include/getsinglegeraet.php
+++ include/getsinglegeraet.php
@@ -89,8 +89,7 @@ include_once("functions.inc.php");
     $sqldata['bemerkung']=nl2br($sqldata['bemerkung']);
     
     $sqldata['bemerkung']=str_replace('strong','b',$sqldata['bemerkung']);
-    //$sqldata['bemerkung']=str_replace('<p>','',$sqldata['bemerkung']);
-    //$sqldata['bemerkung']=str_replace('</p>','',$sqldata['bemerkung']);
+    $sqldata['bemerkung']=str_replace('em','i',$sqldata['bemerkung']);
     
     
     echo '
