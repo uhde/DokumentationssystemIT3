e024da434730519e7c6d39f6cc580ab1e2438b2b
uhde <micha.grueninger@uhde.de>
2013-07-17 11:12:33 +0200

    test

--- layout/edit_dokumente.lay.php
+++ layout/edit_dokumente.lay.php
@@ -47,7 +47,7 @@
     <td align="center" colspan="2">
         <input type="submit" value="Speichern und Schließen" class="button1">
         
-        <input type="button" value="Abbrechen" onClick="javascript:void(top.Boxy.linkedTo( top.$('#kundenwahl')[0]).hide());" class="button1">
+        <input type="button" value="Abbrechen" onClick="javascript:void(parent.$.fancybox.close());" class="button1">
     </td>
 </tr>
 </form>
