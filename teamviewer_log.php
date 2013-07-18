3cc002a26885b9ca025444487b37521784fe083c
uhde <micha.grueninger@uhde.de>
2013-07-18 09:32:11 +0200

    der zweite timestamp wird generiert

--- teamviewer_log.php
+++ teamviewer_log.php
@@ -11,6 +11,11 @@ $uhrzeit1 = explode(':',$logteile[2]);
 echo "d:".$datum1[0]." month:".$datum1[1]." year:".$datum1[2].'<br>';
 echo "h:".$uhrzeit1[0]."   m:".$uhrzeit1[1]."  s:".$uhrzeit1[2].'<br>';
 $timestamp1 = mktime($uhrzeit1[0],$uhrzeit1[1],$uhrzeit1[2],$datum1[1],$datum1[0],$datum1[2]);
-echo $timestamp1;
-echo date(DATE_RFC822,$timestamp1);
+echo $timestamp1.'<br>';
+//echo "<br>".date(DATE_RFC822,$timestamp1);
+$datum2 = explode('-',$logteile[3]);
+$uhrzeit2 = explode(':',$logteile[4]);
+$timestamp2 = mktime($uhrzeit2[0],$uhrzeit2[1],$uhrzeit2[2],$datum2[1],$datum2[0],$datum2[2]);
+echo $timestamp2.'<br>';
+
 ?>
