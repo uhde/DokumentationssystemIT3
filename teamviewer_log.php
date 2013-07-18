fa0af9bc23100531f6cfe0c840d57c73c41cbb54
uhde <micha.grueninger@uhde.de>
2013-07-18 08:50:29 +0200

    mal mit preg_split ausprobiert

--- teamviewer_log.php
+++ teamviewer_log.php
@@ -1,7 +1,7 @@
 <?php
 // In diesem Script wird der Teamviewer log aus appdata in seine Einzelteile zerlegt und in eine Datenbank eingetragen.
 $logzeile = "216541686                       29-01-2013 13:40:11             29-01-2013 14:54:33             grueninger                      RemoteControl                   {EF6362C2-8AC6-461F-A566-418BEF30E1EE}";
-$logteile = explode(" ",$logzeile);
+$logteile =  preg_split("/[\s,]+/",$logzeile);
 foreach($logteile as $temp)
 {
     echo $temp."<br>";
