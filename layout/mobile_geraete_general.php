2462d962f67e56c19e8fc1202b0e11ab3c77c83d
uhde <micha.grueninger@uhde.de>
2013-07-19 14:55:48 +0200

    layout änderung

---
+++ layout/mobile_geraete_general.php
@@ -0,0 +1,102 @@
+!#----- Header:Begin -----#!
+<div id="frame_devicenav">
+    <table style="padding-right:20px;width:100%;border:none;background-color:#666;border-collapse:collapse;border:1px solid #ddd;" id="DeviceData">
+    <thead>
+    <tr>
+    <th style="width:22%;text-align:center;vertical-align:middle;" class="{$name_sort_order}"><a href="index.php?sort_name=name&sort_order={$sort_order}">Name</a>{$name_IMG}</th>
+    <th style="width:22%;text-align:center;vertical-align:middle;" class="{$adresse_sort_order}"><a href="index.php?sort_name=adresse&sort_order={$sort_order}">IP-Adresse</a>{$ip_adresse_IMG}</th>
+    <th style="width:22%;text-align:center;vertical-align:middle;" class="{$system_sort_order}"><a href="index.php?sort_name=system&sort_order={$sort_order}">Systemtyp</a>{$system_IMG}</th>
+    <th style="width:22%;text-align:center;vertical-align:middle;" class="{$benutzer_sort_order}"><a href="index.php?sort_name=benutzer&sort_order={$sort_order}">Benutzer</a>{$zimmer_IMG}</th>
+
+    <th style="width:4%;">&nbsp;</th>
+    <th style="width:4%;">&nbsp;</th>
+    <th style="width:4%;">&nbsp;</th>
+    <th style="width:200px;">&nbsp;</th>
+    </tr>
+    </thead>
+    </table>
+</div>
+<div id="Buchungen">
+<table style="width:100%;border:none;background-color:#666;border-collapse:collapse;border:1px solid #eee;border-bottom:none;" id="DeviceData">
+!#----- Header:End -----#!
+
+
+!#----- Data:Begin -----#!
+    <tr id="Info{$id}" class="Data{$LineClass}" onmouseover="this.className='Aktiv'"  onmouseout="this.className='Data{$LineClass}'" >
+        <td class="slider" value="#trinfo{$id}" onclick="loadXMLDoc('../include/getsinglegeraet.php','trinfo{$id}','{$id}','{$time}')">
+            {$name}
+        </td>
+        <td class="slider" value="#trinfo{$id}" onclick="loadXMLDoc('http://192.168.200.14/dokuit3/include/getsinglegeraet.php','trinfo{$id}','{$id}','{$time}')">
+            {$ip_adresse}
+        </td>
+        <td class="slider" value="#trinfo{$id}" onclick="loadXMLDoc('../include/getsinglegeraet.php','trinfo{$id}','{$id}','{$time}')">
+            {$system}
+        </td>
+        <td class="slider" value="#trinfo{$id}" onclick="loadXMLDoc('../include/getsinglegeraet.php','trinfo{$id}','{$id}','{$time}')">
+            {$benutzer}
+        </td>
+        <td style="text-align:center;vertical-align:middle;width:4%;border-right:1px solid #aaa;border-top:1px solid #ccc;">
+            <a href="#" onclick="activex.run('ping.exe -n 9 {$ping}');" title="Ping">
+                <img src="../syspics/ping4.png" alt="Ping" width="18" height="13">
+            </a>
+        </td>
+
+        <td style="text-align:center;vertical-align:middle;width:4%;border-top:1px solid #ccc;">
+            <a class="edit" data-fancybox-type="iframe" href="edit_geraete.php?kunde={$kunde}&id={$id}&mode=edit" >
+            <img src="../syspics/edit.png" title="Eintrag bearbeiten">
+            </a>
+        </td>        
+        
+    </tr>
+    <tr >
+        <td colspan=7 class="info_schatten4">
+            <span id="trinfo{$id}" class="TRInfo">
+            
+            </span>
+        </td>
+    </tr>
+!#----- Data:End -----#!
+
+
+
+
+!#----- Keine_Daten:Begin -----#!
+<p class="Keine_Daten">
+Es sind keine {$show_kat}-Geräte bei diesem Kunden eingetragen!
+<br /><br />
+<a class="footer_bt" data-fancybox-type="iframe" href="edit_geraete.php?kunde={$kunde}&mode=create&name=geraete&kategorie={$kategorie}">
+    <img src="../syspics/new_entry.png" class="footer_img" alt="Neuer Eintrag" title="Neuer Eintrag">
+</a>
+</p>
+!#----- Keine_Daten:End -----#!
+
+
+!#----- Footer:Begin -----#!
+</table>
+    <span class="TRInfoCloseall" style="padding-right:10px;">
+        <input type="button"  value="Alle Infos schließen" onClick="ClearAllLoad()"></input>
+    </span>
+</div>
+
+!#----- Footer:End -----#!
+!#----- Footer2:Begin -----#!
+
+<a class="footer_bt" data-fancybox-type="iframe" href="edit_geraete.php?kunde={$kunde}&mode=create&name=geraete&kategorie={$kategorie}">
+    <img src="../syspics/new_entry.png" class="footer_img" alt="Neuer Eintrag" title="Neuer Eintrag">
+</a>
+<span style="paddin:4px;">&nbsp;</span>
+<a class="footer_bt" href="dnsaufloesung.php?site={$site}" id="boxyfoo" > <!--onclick="showboxy('kundenwahl','dnsaufloesung.php','DNS Auflösung','400','200');" -->
+    <img src="../syspics/button_refresh.png" class="footer_img" alt="DNS Refresh" title="IP Adressen aktualisieren">
+</a>
+<span style="paddin:4px;">&nbsp;</span>
+<a class="footer_bt" href="userconf/ipordns.php?site={$site}" id="boxyfoo" > 
+    <img src="../syspics/switch.png" class="footer_img" alt="DNS to IP" title="DNS/IP Name anezeigen">
+</a>
+
+    <!--<td colspan="5" style="background-color:#fff;border-bottom:1px solid #333;padding-left:0px;padding-right:0px;padding-bottom:0px;border-top:1px solid #ddd;">-->
+!#----- Footer2:End -----#!
+
+!#----- Info:Begin -----#!
+
+!#----- Info:End -----#!
+<!-- Ende des Templates -->
