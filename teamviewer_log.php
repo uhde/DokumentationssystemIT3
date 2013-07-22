c4cf5f2f3b0bcabead124f4ec818d81619a8d0c5
uhde <micha.grueninger@uhde.de>
2013-07-22 14:38:15 +0200

    vergleichsoperator geändert

--- teamviewer_log.php
+++ teamviewer_log.php
@@ -42,7 +42,7 @@
         $test = $objMySQL->Query($sql);
         echo $sql."<br>";
         echo intval(mysql_num_rows($test))."<br>";
-        if(intval(mysql_num_rows($test))!=1) {
+        if(intval(mysql_num_rows($test))<1) {
             // Wenn der Datensatz nicht vorhanden sein sollte, wird dieser Teil der if anweisung ausgeführt.
             //Sucht die Kundennummer raus.
             $sql = "SELECT kunde FROM geraete AS gr, geraete_login AS gl WHERE gl.login=".teamviewer_id." AND gl.geraete_id=gr.id";
