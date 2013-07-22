a24cd9da81c7f6185bf78d4afba7a6f9d15ad7dc
uhde <micha.grueninger@uhde.de>
2013-07-22 14:53:52 +0200

    es wird abgeprüft ob das feld teamviewerid leer ist.

--- teamviewer_log.php
+++ teamviewer_log.php
@@ -44,18 +44,20 @@
         
         echo intval(mysql_num_rows($test))."<br>";
         if(intval(mysql_num_rows($test))<1) {
-            // Wenn der Datensatz nicht vorhanden sein sollte, wird dieser Teil der if anweisung ausgeführt.
-            //Sucht die Kundennummer raus.
-            $sql = "SELECT kunde FROM geraete AS gr, geraete_login AS gl WHERE gl.login=".teamviewer_id." AND gl.geraete_id=gr.id";
-            $tempdata = $objMySQL->QuerySingleRowArray($sql);
-            $kunde = $tempdata['kunde'];
-            
-            $sql = "INSERT INTO `".DB_DATABASE."`.`teamviewer_log` SET ";
-            $sql = $sql."teamviewer_id='".$teamviewer_id."' , start_zeit='".$timestamp_anfang."' , end_zeit='".$timestamp_ende."' , ";
-            $sql = $sql."benutzer='".$benutzer."' , kunde='".$kunde."' , dauer='".$dauer."'";
-            $objMySQL->Query($sql);
-            echo $sql."<br>";
-            $count ++;
+            if(!empty($teamviewer_id))
+                // Wenn der Datensatz nicht vorhanden sein sollte, wird dieser Teil der if anweisung ausgeführt.
+                //Sucht die Kundennummer raus.
+                $sql = "SELECT kunde FROM geraete AS gr, geraete_login AS gl WHERE gl.login=".teamviewer_id." AND gl.geraete_id=gr.id";
+                $tempdata = $objMySQL->QuerySingleRowArray($sql);
+                $kunde = $tempdata['kunde'];
+                
+                $sql = "INSERT INTO `".DB_DATABASE."`.`teamviewer_log` SET ";
+                $sql = $sql."teamviewer_id='".$teamviewer_id."' , start_zeit='".$timestamp_anfang."' , end_zeit='".$timestamp_ende."' , ";
+                $sql = $sql."benutzer='".$benutzer."' , kunde='".$kunde."' , dauer='".$dauer."'";
+                $objMySQL->Query($sql);
+                echo $sql."<br>";
+                $count ++;
+            }
         }
        echo "----------------<br>";
         
