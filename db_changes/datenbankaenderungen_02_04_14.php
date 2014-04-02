03280c259b320ef41587d88163e7d20d82730e3a
uhde <micha.grueninger@uhde.de>
2014-04-02 15:14:15 +0200

    SQL Patch, siehe beschreibung
    Löscht die Felder Router und FTPdir in der Tabelle geräte

---
+++ db_changes/datenbankaenderungen_02_04_14.php
@@ -0,0 +1,22 @@
+<html><head></head>
+<body>
+working....
+<?php
+
+require('../include/config.inc.php');
+include("../include/mysql.class.php");
+include("../include/template.class.php");
+include("../include/functions.inc.php");
+    $objMySQL = new MySQL();
+    if (!$objMySQL->Open(DB_DATABASE, DB_SERVER, DB_USER, DB_PASSWORD)) {
+       echo $objMySQL->Error();
+       $objMySQL->Kill();
+    }
+    
+    $sql = "ALTER TABLE `geraete` DROP `router`,  DROP `ftpdir`;";
+    $objMySQL->Query($sql);
+    
+    echo "Scriptende";
+?>
+</body>
+</html>
