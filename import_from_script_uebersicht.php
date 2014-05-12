eab9addd071d507566685c9cc50ddb5b5d3f308b
uhde <micha.grueninger@uhde.de>
2014-05-12 16:19:44 +0200

    Header angepasst

--- import_from_script_uebersicht.php
+++ import_from_script_uebersicht.php
@@ -1,7 +1,10 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-
+//error_reporting(E_ALL);
+//ini_set('display_errors', TRUE);
+require('include/config.inc.php');
+include("include/mysql.class.php");
+include("include/template.class.php");
+include("include/functions.inc.php");
     
     $datei = "NeueRechner";
     $basis_url = "http://uhdsrv14.uhde.de/systeminvent/";
@@ -9,20 +12,25 @@ ini_set('display_errors', TRUE);
     $liste =  file($url, FILE_IGNORE_NEW_LINES );
     echo $liste;
 ?>
-<html><head></head>
+<html>
+    <head>
+        <meta http-equiv="x-ua-compatible" content="ie=9">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <link rel="stylesheet" href="css/reset.css" type="text/css" />
+    </head>
 <body>
-<h1>TESTBETRIEB<h1>
-<table>
-<?php
-
-    foreach($liste as $rechner)
-    {
-        echo "<tr><td>".$rechner."</td>";
-            echo '<td><a href="import_from_script_text_file.php?rechner='.$rechner.'&kunde='.$_GET['kunde'].'&kategorie='.$_GET['kategorie'].' ">hinzufügen</td>';
-            echo '<td><a href="uhdsr.php?rechner='.$rechner.'">TODO LINK loeschen</td>';
-        echo "</tr>";
-    }
-?>
-</table>
+    <h1>TESTBETRIEB<h1>
+    <table>
+        <?php
+            foreach($liste as $rechner)
+            {
+                echo "<tr><td>".$rechner."</td>";
+                    echo '<td><a href="import_from_script_text_file.php?rechner='.$rechner.'&kunde='.$_GET['kunde'].'&kategorie='.$_GET['kategorie'].' ">hinzufügen</td>';
+                    echo '<td><a href="uhdsr.php?rechner='.$rechner.'">TODO LINK loeschen</td>';
+                echo "</tr>";
+            }
+        ?>
+    </table>
 </body>
 </html>
