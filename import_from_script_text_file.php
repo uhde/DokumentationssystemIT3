8d167cfef6aa4758968d9665fae85c7a81018dc4
uhde <micha.grueninger@uhde.de>
2014-05-13 09:58:50 +0200

    Programme hinzugefügt, DB Änderungen aktiviert

--- import_from_script_text_file.php
+++ import_from_script_text_file.php
@@ -1,6 +1,14 @@
-<html><head></head>
+<html>
+    <head>
+        <meta http-equiv="x-ua-compatible" content="ie=9">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <link rel="stylesheet" href="css/reset.css" type="text/css" />
+    </head>
 <body>
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
 require('include/config.inc.php');
 include("include/mysql.class.php");
 include("include/template.class.php");
@@ -18,7 +26,11 @@ include("include/functions.inc.php");
     $systemname = $_GET['rechner'];
     $kategorie = $_GET['kategorie'];
     $kunde = $_GET['kunde'];
+    $tv_prog_id = 15;
+    $bios_prog_id = 22;
+    $geraetepw_prog_id = 20;
     
+    $kundenkuerzel = substr($systemname,0,3);
     // Für das unten stehende Script werden folgende Daten benötigt:
     // kunde, kategorie, systemname
     
@@ -36,16 +48,54 @@ include("include/functions.inc.php");
     $seriennummer=$invent_row[2];
     $mac=$invent_row[3];
     $teamviewer_id=$invent_row[4];
+    $bs=$invent_row[4];             //Betriebssystem
     $sqlquery="INSERT INTO `".DB_DATABASE."`.`".$tabelle."` SET ";
-    $sqlquery=$sqlquery."`name` = '".$rechnername."' ";
-    $sqlquery=$sqlquery."`kunde` = '".$kunde."' ";
-    $sqlquery=$sqlquery."`kategorie` = '".$kategorie."' ";
-    $sqlquery=$sqlquery."`system` = '".$systemtyp."' ";
-    $sqlquery=$sqlquery."`sn` = '".$seriennummer."' ";
-    $sqlquery=$sqlquery."`mac_adresse` = '".$mac."' ";
+    $sqlquery=$sqlquery."`name` = '".$rechnername."', ";
+    $sqlquery=$sqlquery."`kunde` = '".$kunde."', ";
+    $sqlquery=$sqlquery."`kategorie` = '".$kategorie."', ";
+    $sqlquery=$sqlquery."`system` = '".$systemtyp."', ";
+    $sqlquery=$sqlquery."`sn` = '".$seriennummer."', ";
+    $sqlquery=$sqlquery."`mac_adresse` = '".$mac."', ";
+    $sqlquery=$sqlquery."`bs` = '".$bs."' ";
+    $geraete_einfuegen = $sqlquery; // Zu Übersichtszwecken
     
+    $objMySQL->Query($geraete_einfuegen);
+    $geraete_id = mysql_insert_id(); //Die Geräte ID wird geholt
     
-    echo $sqlquery;
+//    http://dev.mysql.com/doc/refman/5.1/de/information-functions.html
+    $sql="INSERT INTO `".DB_DATABASE."`.`".$tabelle."` SET ";
+    $sql=$sql."`geraete_id` = '".$geraete_id."', ";
+    $sql=$sql."`programm_id` = '".$tv_prog_id."', ";
+    $sql=$sql."`login` = '".$teamviewer_id."', ";
+    $sql=$sql."`passwort` = 'UHDETV', ";
+    $sql=$sql."`aktiv` = '1' ";
+    $teamviewer_insert = $sql;
+    
+    $sql="INSERT INTO `".DB_DATABASE."`.`".$tabelle."` SET ";
+    $sql=$sql."`geraete_id` = '".$geraete_id."', ";
+    $sql=$sql."`programm_id` = '".$bios_prog_id."', ";
+    $sql=$sql."`login` = '', ";
+    $sql=$sql."`passwort` = '$".$kundenkuerzel."bios%"."', ";
+    $sql=$sql."`aktiv` = '0' ";
+    $bios_insert = $sql;
+    
+    $sql="INSERT INTO `".DB_DATABASE."`.`".$tabelle."` SET ";
+    $sql=$sql."`geraete_id` = '".$geraete_id."', ";
+    $sql=$sql."`programm_id` = '".$geraetepw_prog_id."', ";
+    $sql=$sql."`login` = '".$kundenkuerzel."wsadmin', ";
+    $sql=$sql."`passwort` = '$".$kundenkuerzel."wslan%"."', ";
+    $sql=$sql."`aktiv` = '0' ";
+    $geraetepw_insert = $sql; //Lokaler Login
+    
+    $objMySQL->Query($teamviewer_insert);
+    $objMySQL->Query($bios_insert);
+    $objMySQL->Query($geraetepw_insert);
+    echo "Geraete Einfuegen SQL: ".$geraete_einfuegen."<br>";  
+    echo "Geraete ID: ".$geraete_id."<br>";
+    echo $teamviewer_insert."<br>";  
+    echo $bios_insert."<br>"; 
+    echo $geraetepw_insert."<br>"; 
+    
     
     //$objMySQL->Query($sqlquery);
 ?>
