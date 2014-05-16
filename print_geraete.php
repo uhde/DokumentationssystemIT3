a1890b8f2f3b21410275ca69b5623b16c7219a71
uhde <micha.grueninger@uhde.de>
2014-05-16 15:27:03 +0200

    js header eingefügt.

--- print_geraete.php
+++ print_geraete.php
@@ -4,6 +4,64 @@
         <!--<meta http-equiv="content-type" content="text/html; charset=utf-8">-->
         <link rel="stylesheet" href="css/styles.css" type="text/css" />
         <link rel="stylesheet" href="css/reset.css" type="text/css" />
+        
+        <script type='text/javascript'>
+        // Diese Funktion lädt die spezifischen Daten einer Sache nach, und fügt dies dann in den dafür
+        // vorgesehenen Platz ein.
+        function loadXMLDoc(seite,jq_aufzurufende_id,id,time)
+        {
+            var xmlhttp;
+            var timejs = new Date().getTime();
+            xmlhttp=new XMLHttpRequest();
+            xmlhttp.onreadystatechange=function()
+            {
+                // Wenn die website mit ajax geladen wurde, wird diese If Anweisung ausgeführt
+                // Dort wird dann der geladene Inhalt erst in das Dokument eingebaut
+                if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                {
+                    var aufzurufende_id;
+                    aufzurufende_id = "#"+jq_aufzurufende_id;
+                    // Hier wird das Feld geleert, bevor etwas neues reingeschrieben wird. 
+                    // Dies wird vor allem gemacht, um Fehler im IE vorzubeugen.
+                    $(aufzurufende_id).empty();
+                    // Hier wird das Feld befüllt
+                    $(aufzurufende_id).html(xmlhttp.responseText);
+                    //document.getElementById().innerHTML = "Dies ist ein test";
+                    //alert(xmlhttp.responseText);
+                }
+            }
+            // Der eigentliche Request.
+            xmlhttp.open("GET",seite+"?id="+id+"&time="+timejs,true);
+            //alert(seite+"?id="+id+"&time="+time);
+            xmlhttp.send();
+        }
+        function ClearLoad(Test)
+        {
+            var aufzurufende_id;
+            aufzurufende_id = "#"+Test;
+            $(aufzurufende_id).html("");
+        }
+        function ClearAllLoad()
+        {
+            $(".TRInfo").text(" ");
+        }
+        
+        function slider_generell() {
+            $(this).toggleClass("up");
+            name=$(this).attr("value");
+           // $("#Buchungen .TRInfo").hide();
+            if($(this).hasClass('up')) {
+                $(name).show();
+            }else{
+                $(name).hide();
+            }
+            //Funktioniert nicht... wieso?
+            //vll wegen. $(this)
+        }
+        </script>
+        
+        
+        
     </head>
     <body>
 
