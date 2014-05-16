4e18e40720c0efa300b4eb3ebc91a9ef0cb5fa59
uhde <micha.grueninger@uhde.de>
2014-05-16 15:03:55 +0200

    pfad angaben von include dateien angepasst

--- print_geraete.php
+++ print_geraete.php
@@ -10,16 +10,16 @@
     <?php 
     error_reporting(E_ALL);
     ini_set('display_errors', TRUE);
-    require_once('config.inc.php');
-    include_once("mysql.class.php");
-    include_once("template.class.php");
-    include_once("functions.inc.php");
+    require_once('include/config.inc.php');
+    include_once("include/mysql.class.php");
+    include_once("include/template.class.php");
+    include_once("include/functions.inc.php");
         $objMySQL = new MySQL();
         if (!$objMySQL->Open(DB_DATABASE, DB_SERVER, DB_USER, DB_PASSWORD)) {
            echo $objMySQL->Error();
            $objMySQL->Kill();
         }
-    include ("geraete.inc.php");
+    include ("include/geraete.inc.php");
 
 
     ?>
