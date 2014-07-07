a96b78ebebe997e342c4d2417320a7cb04feab3d
uhde <micha.grueninger@uhde.de>
2014-07-07 10:53:55 +0200

    vergesssenes if entfernt

--- fernwartungs_log/dokuit_log.php
+++ fernwartungs_log/dokuit_log.php
@@ -56,7 +56,7 @@ ini_set('display_errors', TRUE);
                 $timestamp_anfang   = mktime(intval($uhrzeit1[0]),intval($uhrzeit1[1]),0,intval($datum1[1]),intval($datum1[0]),intval($datum1[2]));
                 $timestamp_ende     = $timestamp_anfang + $dauer;
                 
-                if (
+                
                 foreach( $zeile as $logzeile )
                 {
                     echo $logzeile.", ";
@@ -73,81 +73,7 @@ ini_set('display_errors', TRUE);
         fclose($handle);
     }
     
-    // Teamviewer - LOG
-    /*
-    foreach( $log as $logzeile ) 
-    {
-        $zeile_verarbeitet = true;
-        // Hier wird eine Zeile des Logs aufgesplittet. Struktur: 0=tv-id 1=datum1 2=uhrzeit1
-        $logteile =  preg_split("/[\s,]+/",$logzeile);
-        //foreach($logteile as $temp)
-        //{
-        //    echo $temp."<br>";
-        //}
-        $datum1 = explode('-',$logteile[1]);
-        $uhrzeit1 = explode(':',$logteile[2]);
-        //echo "d:".$datum1[0]." month:".$datum1[1]." year:".$datum1[2].'<br>';
-        //echo "h:".$uhrzeit1[0]."   m:".$uhrzeit1[1]."  s:".$uhrzeit1[2].'<br>';
-        $timestamp_anfang = mktime(intval($uhrzeit1[0]),intval($uhrzeit1[1]),intval($uhrzeit1[2]),intval($datum1[1]),intval($datum1[0]),intval($datum1[2]));
-        //echo $timestamp_anfang.'<br>';
-        //echo "<br>".date(DATE_RFC822,$timestamp_anfang);
-        $datum2 = explode('-',$logteile[3]);
-        $uhrzeit2 = explode(':',$logteile[4]);
-        $timestamp_ende = mktime(intval($uhrzeit2[0]),intval($uhrzeit2[1]),intval($uhrzeit2[2]),intval($datum2[1]),intval($datum2[0]),intval($datum2[2]));
-        //echo $timestamp_ende.'<br>';
-        $benutzer = $logteile[5];
-        $teamviewer_id = $logteile[0];
-        
-        $dauer = $timestamp_ende - $timestamp_anfang;
-        
-        // Hier wird geprüft, ob es einen solchen eintrag schon gibt
-        $sql = 'SELECT * FROM '.DB_DATABASE.".".$log_tabelle." WHERE ziel = '".$teamviewer_id."' AND start_zeit='".$timestamp_anfang."' AND end_zeit='".$timestamp_ende."'";
-        $test = $objMySQL->Query($sql);
-        //echo $sql."<br>";
-        
-        //echo intval(mysql_num_rows($test))."<br>";
-        if(intval(mysql_num_rows($test))<1) {
-            if(!empty($teamviewer_id)){
-                // Wenn der Datensatz nicht vorhanden sein sollte, wird dieser Teil der if anweisung ausgeführt.
-                //Sucht die Kundennummer raus.
-                
-                //Prüft ob eine Teamviewer-Lan verbindung vorlag, oder nicht
-                if(strlen($teamviewer_id)==9 && ctype_digit($teamviewer_id))
-                {
-                    // Falls eine Teamviewer Web verbindung vorlag, wird dieser Code Teil ausgeführt
-                    $sql = "SELECT gr.kunde FROM geraete AS gr, geraete_login AS gl WHERE gl.login='".$teamviewer_id."' AND gl.geraete_id=gr.id";
-                    $tempdata = $objMySQL->QuerySingleRowArray($sql);
-                    
-                    $kunde = $tempdata['kunde'];
-                    $programm = "Teamviewer - Web";
-                    
-                }
-                else {
-                    $sql = "SELECT gr.kunde FROM geraete AS gr WHERE gr.adresse='".$teamviewer_id."' ";
-                    $tempdata = $objMySQL->QuerySingleRowArray($sql);
-                    
-                    $kunde = $tempdata['kunde'];
-                    $programm = "Teamviewer - LAN";
-                }
-                // Falls kein Kunde gefunden werden kann, wird er auf minus 1 gesetzt
-                if (empty($kunde))
-                    $kunde = -1;
-                // Debug-Ausgabe
-                //echo "<br>".$sql." : <br>";
-                
-                $sql = "INSERT INTO `".DB_DATABASE."`.`".$log_tabelle."` SET ";
-                $sql = $sql."ziel='".$teamviewer_id."' , start_zeit='".$timestamp_anfang."' , end_zeit='".$timestamp_ende."' , ";
-                $sql = $sql."benutzer='".$benutzer."' , kunde='".$kunde."' , dauer='".$dauer."' , programm = '".$programm."'";
-                $objMySQL->Query($sql);
-                //echo $sql."<br>";
-                $count ++;
-            }
-        }
-       //echo "----------------<br>";
-        
-        
-    }
-    */
+
     if ($zeile_verarbeitet == true)
         echo "Die Datei wurde erfolgreich eingelesen.";
     else
