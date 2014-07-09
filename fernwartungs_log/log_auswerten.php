8cc9de8df00d990998cb9c0948f156578ff3eefe
uhde <micha.grueninger@uhde.de>
2014-07-09 14:23:16 +0200

    eine einfache auswertungsausgabe (csv)

---
+++ fernwartungs_log/log_auswerten.php
@@ -0,0 +1,35 @@
+<?php
+// Dieses Script hat den Zweck die Logdaten auszugeben
+
+//error_reporting(E_ALL);
+//ini_set('display_errors', TRUE);
+    require_once('../include/config.inc.php');
+    include_once("../include/mysql.class.php");
+    include_once("../include/template.class.php");
+    include_once("../include/functions.inc.php");
+
+    $objMySQL = new MySQL();
+    if (!$objMySQL->Open(DB_DATABASE, DB_SERVER, DB_USER, DB_PASSWORD)) {
+       echo $objMySQL->Error();
+       $objMySQL->Kill();
+    
+    $kunde = $_GET["kunde"];
+    $starttime = $_GET["starttime"];
+    $endtime = $_GET["endtime"];
+    
+    $sql = "SELECT * FROM fernwartungs_log WHERE ";
+    $sql = $sql."kunde='".$kunde."' AND ";
+    $sql = $sql."start_zeit<'".$endtime."' AND ";
+    $sql = $sql."start_zeit>'".$starttime."'";
+    $sql = $sql.";";
+    $arr_data =  $objMySQL->QueryArray($sql,MYSQL_ASSOC);  
+    foreach($arr_data as $zeile)
+    {
+        foreach($zeile as $eintrag)
+        {
+            echo '"'$eintrag.'";';
+        }
+        echo "<br>";
+    }
+    
+?>
