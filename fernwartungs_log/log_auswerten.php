881804a8583c249ef15986d464af9fcf8e7cdb1d
uhde <micha.grueninger@uhde.de>
2014-07-09 14:35:09 +0200

    debug optionen an geschaltet

--- fernwartungs_log/log_auswerten.php
+++ fernwartungs_log/log_auswerten.php
@@ -1,8 +1,8 @@
 <?php
 // Dieses Script hat den Zweck die Logdaten auszugeben
 
-//error_reporting(E_ALL);
-//ini_set('display_errors', TRUE);
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
     require_once('../include/config.inc.php');
     include_once("../include/mysql.class.php");
     include_once("../include/template.class.php");
@@ -14,13 +14,13 @@
        $objMySQL->Kill();
     
     $kunde = $_GET["kunde"];
-    $starttime = $_GET["starttime"];
-    $endtime = $_GET["endtime"];
+  //  $starttime = $_GET["starttime"];
+    //$endtime = $_GET["endtime"];
     
     $sql = "SELECT * FROM fernwartungs_log WHERE ";
-    $sql = $sql."kunde='".$kunde."' AND ";
-    $sql = $sql."start_zeit<'".$endtime."' AND ";
-    $sql = $sql."start_zeit>'".$starttime."'";
+    $sql = $sql."kunde='".$kunde."'";
+   // $sql = $sql." AND start_zeit<'".$endtime."' AND ";
+  //  $sql = $sql."start_zeit>'".$starttime."'";
     $sql = $sql.";";
     $arr_data =  $objMySQL->QueryArray($sql,MYSQL_ASSOC);  
     foreach($arr_data as $zeile)
